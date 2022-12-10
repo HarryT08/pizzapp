940b61d2db503793b965b1071a847230440dcb96
Felipe Alferez <alferezfelipe@gmail.com>
2022-12-10 14:51:26 -0500

    Creada funcionalidad obtener productos

--- backend/src/controllers/products-controller.ts
+++ backend/src/controllers/products-controller.ts
@@ -1,13 +1,6 @@
 import { Request, Response } from "express";
-import { MateriaPrima } from "../entities/MateriaPrima";
 import { Preparacion } from "../entities/Preparacion";
 import { Producto } from "../entities/Producto";
-
-// export async function getProducts(req:Request, res:Response): Promise<Response>{
-//     const conn = await connect();
-//     const product = await conn.query('SELECT * FROM producto');
-//     return res.send(product[0]);
-// }
 
 export const getProduct = async (req: Request, res: Response) => {
   const { id } = req.params;
@@ -20,6 +13,17 @@ export const getProduct = async (req: Request, res: Response) => {
   return res.json(product);
 };
 
+export const getProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await Producto.find();  
+    return res.json(products);
+  } catch (error) {
+    return res.json({
+      "mensaje" : "Ha ocurrido algo inesperado"
+    }) 
+  }
+}
+
 export const createProduct = async (req: Request, res: Response) => {
   let { nombre, precio, presentaciones } = req.body;
   const producto = new Producto();
@@ -29,18 +33,18 @@ export const createProduct = async (req: Request, res: Response) => {
   return res.json('Producto creado');
 };
 
-const createPreparation =  (idProducto: number, presentaciones: any) => {
-  let data : Preparacion[] = []
+function createPreparation(idProducto: number, presentaciones: any) {
+  let data : Preparacion[] = [];
   presentaciones.forEach((presentacion: any) => {
-    const size = presentacion["tamaño"]
-    presentacion["ingredientes"].forEach( (ingrediente: any) => {
-      const idMateria = ingrediente["id"]
-      const cantidad = ingrediente["cantidad"]
+    const size = presentacion["tamaño"];
+    presentacion["ingredientes"].forEach((ingrediente: any) => {
+      const idMateria = ingrediente["id"];
+      const cantidad = ingrediente["cantidad"];
       const preparacion = new Preparacion();
       preparacion.init(idProducto, idMateria, size, cantidad);
-      data.push(preparacion)
-    })
-  })
+      data.push(preparacion);
+    });
+  });
   console.log(data);
   Preparacion.save(data);
 }
